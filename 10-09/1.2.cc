c27ed0648abc87154a4ce208f3fc91822354a456
tomasel <elia.tomasi@pm.me>
2023-10-09 15:02:15 +0200

    rimossa variabile m

--- 10-09/1.2.cc
+++ 10-09/1.2.cc
@@ -14,11 +14,10 @@ using namespace std;
 
 int main(){
     int n = 7;
-    int m = 7;
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(i == j || i == n-j-1 || i == n / 2 || j == m/2)
+        for(int j = 0; j < n; j++){
+            if(i == j || i == n-j-1 || i == n / 2 || j == n/2)
                 cout << "x ";
             else
                 cout << "  ";
