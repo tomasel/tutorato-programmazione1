5f91d5dc5eab3fcf82dc8603ac2f8b3e13c711be
tomasel <elia.tomasi@pm.me>
2023-10-09 11:33:51 +0200

    sol es 5

---
+++ 10-09/5.cc
@@ -0,0 +1,30 @@
+/*
+ Scrivere una funzione ricorsiva che calcoli la somma tra due numeri interi positivi
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int sommaRic(int a, int b) {
+  if (a == 0)
+    return b;
+  if (b == 0)
+    return a;
+  return sommaRic(a - 1, b - 1) + 2;
+}
+
+int main () {
+
+  int a, b;
+  
+  cout << "a: ";
+  cin >> a;
+
+  cout << "b: ";
+  cin >> b;
+
+  cout << sommaRic(a, b) << endl;
+
+  return 0;
+}
