7f5e61f783781db9f078ea1cc4eb96464e467388
tomasel <elia.tomasi@pm.me>
2023-10-16 14:46:23 +0200

    code restructure

--- 10-16/4.cc
+++ 10-16/4.cc
@@ -1,6 +1,6 @@
 
 /*
-Data una media iniziale e un numero di crediti totali acquisiti, far inserire all'utente il voto e i crediti di un nuovo esame e scrivere una funzione per ricalcolare la nuova media pesata.
+   Data una media iniziale e un numero di crediti totali acquisiti, far inserire all'utente il voto e i crediti di un nuovo esame e scrivere una funzione per ricalcolare la nuova media pesata.
 
 Bonus: Scrivere una funzione con passaggio per valore, una con passaggio per riferimento e una con passaggio per puntatore. Provare ad eseguire ognuna delle funzioni e capire cosa succede.
 */
@@ -11,46 +11,46 @@ Bonus: Scrivere una funzione con passaggio per valore, una con passaggio per rif
 using namespace std;
 
 float ricalcolaMedia(float media, int crediti, int voto, int creditiEsame){
-    return (media * crediti + voto * creditiEsame) / (crediti + creditiEsame);
+  return (media * crediti + voto * creditiEsame) / (crediti + creditiEsame);
 }
 
 float ricalcolaMedia2(float &media, int &crediti, int voto, int creditiEsame){
-    media = (media * crediti + voto * creditiEsame) / (crediti + creditiEsame);
-    return media;
+  media = (media * crediti + voto * creditiEsame) / (crediti + creditiEsame);
+  return media;
 }
 
 float ricalcolaMedia3(float *media, int *crediti, int voto, int creditiEsame){
-    *media = (*media * *crediti + voto * creditiEsame) / (*crediti + creditiEsame);
-    return *media;
+  *media = (*media * *crediti + voto * creditiEsame) / (*crediti + creditiEsame);
+  return *media;
 }
 
 int main(){
-    
-        float media = 0;
-        int crediti = 0;
-    
-        cout << "Inserisci la media iniziale: ";
-        cin >> media;
-    
-        cout << "Inserisci i crediti totali: ";
-        cin >> crediti;
-    
-        while (true) {
-            int voto = 0;
-            int creditiEsame = 0;
-        
-            cout << "Inserisci il voto dell'esame: ";
-            cin >> voto;
-        
-            cout << "Inserisci i crediti dell'esame: ";
-            cin >> creditiEsame;
-        
-            media = ricalcolaMedia2(media, crediti, voto, creditiEsame);
-            crediti += creditiEsame;
-        
-            cout << "La nuova media è: " << media << endl;
-        }
-        
-    
-        return 0;
+
+  float media = 0;
+  int crediti = 0;
+
+  cout << "Inserisci la media iniziale: ";
+  cin >> media;
+
+  cout << "Inserisci i crediti totali: ";
+  cin >> crediti;
+
+  while (true) {
+    int voto = 0;
+    int creditiEsame = 0;
+
+    cout << "Inserisci il voto dell'esame: ";
+    cin >> voto;
+
+    cout << "Inserisci i crediti dell'esame: ";
+    cin >> creditiEsame;
+
+    media = ricalcolaMedia2(media, crediti, voto, creditiEsame);
+    crediti += creditiEsame;
+
+    cout << "La nuova media è: " << media << endl;
+  }
+
+
+  return 0;
 }
--- 10-16/5.cc
+++ 10-16/5.cc
@@ -1,7 +1,7 @@
 
 /*
 
-Dati in input tre interi positivi in tre variabili (n1, n2, n3), scrivere una funzione che con una procedura “ri-ordini” i numeri in ordine crescente.
+   Dati in input tre interi positivi in tre variabili (n1, n2, n3), scrivere una funzione che con una procedura “ri-ordini” i numeri in ordine crescente.
 
 Bonus: Riscrivere la funzione usando il passaggio di parametri per riferimento.
 
@@ -13,61 +13,61 @@ Bonus: Riscrivere la funzione usando il passaggio di parametri per riferimento.
 using namespace std;
 
 void printInOrdine(int n1, int n2, int n3){
-    if (n1 > n2) {
-        int tmp = n1;
-        n1 = n2;
-        n2 = tmp;
-    }
+  if (n1 > n2) {
+    int tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+  }
 
-    if (n2 > n3) {
-        int tmp = n2;
-        n2 = n3;
-        n3 = tmp;
-    }
+  if (n2 > n3) {
+    int tmp = n2;
+    n2 = n3;
+    n3 = tmp;
+  }
 
-    if (n1 > n2) {
-        int tmp = n1;
-        n1 = n2;
-        n2 = tmp;
-    }
+  if (n1 > n2) {
+    int tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+  }
 
-    cout << n1 << " " << n2 << " " << n3 << endl;
+  cout << n1 << " " << n2 << " " << n3 << endl;
 }
 
 // funzione con parametri per riferimento
 void printInOrdine2(int &n1, int &n2, int &n3){
-    if (n1 > n2) {
-        int tmp = n1;
-        n1 = n2;
-        n2 = tmp;
-    }
+  if (n1 > n2) {
+    int tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+  }
 
-    if (n2 > n3) {
-        int tmp = n2;
-        n2 = n3;
-        n3 = tmp;
-    }
+  if (n2 > n3) {
+    int tmp = n2;
+    n2 = n3;
+    n3 = tmp;
+  }
 
-    if (n1 > n2) {
-        int tmp = n1;
-        n1 = n2;
-        n2 = tmp;
-    }
+  if (n1 > n2) {
+    int tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+  }
 
-    cout << n1 << " " << n2 << " " << n3 << endl;
+  cout << n1 << " " << n2 << " " << n3 << endl;
 }
 
 
 int main() {
 
-    int n1, n2, n3;
-    cout << "Inserisci tre numeri interi positivi: ";
-    cin >> n1 >> n2 >> n3;
+  int n1, n2, n3;
+  cout << "Inserisci tre numeri interi positivi: ";
+  cin >> n1 >> n2 >> n3;
 
-    printInOrdine(n1, n2, n3);
+  printInOrdine(n1, n2, n3);
 
-    printInOrdine2(n1, n2, n3);
+  printInOrdine2(n1, n2, n3);
 
-    return 0;
+  return 0;
 }
 
