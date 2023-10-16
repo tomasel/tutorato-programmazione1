ea65820ce223c68d07cc8e879cd26f5e5d8e3345
tomasel <elia.tomasi@pm.me>
2023-10-16 15:21:25 +0200

    aggiunte soluzioni prodotto e potenza

--- 10-16/2.cc
+++ 10-16/2.cc
@@ -15,6 +15,25 @@ int sommaRic(int a, int b) {
   return sommaRic(a - 1, b - 1) + 2;
 }
 
+int prodotto(int a, int b) {
+  if (b == 0)
+    return 0;
+  return prodotto(a, b - 1) + a;
+}
+
+int prodottoRic(int a, int b) {
+  if (a > b)
+    return prodotto(a, b);
+  else
+    return prodotto(b, a);
+}
+
+int potenzaRic(int base, int esponente) {
+  if (esponente == 0)
+    return 1;
+  return potenzaRic(base, esponente - 1) * base;
+}
+
 int main () {
 
   int a, b;
@@ -25,7 +44,23 @@ int main () {
   cout << "b: ";
   cin >> b;
 
-  cout << sommaRic(a, b) << endl;
+  cout <<
+    a <<
+    " + " <<
+    b <<
+    " = " <<
+    sommaRic(a, b) << endl;
+  cout <<
+    a <<
+    " * " <<
+    b <<
+    " = " <<
+    prodottoRic(a, b) << endl;  // usa una funzione d'appoggio per fare una piccola ottimizzazione
+  cout <<
+    a <<
+    " ^ " <<
+    b << 
+    " = " << potenzaRic(a, b) << endl;
 
   return 0;
 }
