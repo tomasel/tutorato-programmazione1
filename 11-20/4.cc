e367db0855fd587a34a3c31280308ff3be229d11
tomasel <elia.tomasi@pm.me>
2023-11-19 20:24:56 +0100

    sol 4

---
+++ 11-20/4.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+
+typedef struct dataStruct {
+  int* data;
+  int size, numData;
+} dataStruct;
+
+// inserzione di un elemento
+void insertInHeap (dataStruct *pHeap, int newItem) {
+  int index = ++(pHeap->numData);
+  while (index > 1 &&
+         pHeap->data[(int)(index / 2)] < newItem) {
+    pHeap->data[index] = pHeap->data[(int)(index / 2)];
+    index = (int)(index / 2);
+  }
+  pHeap->data[index] = newItem;
+}
+
+// rimozione di un elemento (ricorsiva)
+void heapify(int index, dataStruct *pHeap) {
+  int left, right, max;
+  int tmp;
+
+  left = 2 * index;
+  if (left <= pHeap->numData) {
+    if (pHeap->data[left] > pHeap->data[index])
+      max = left;
+    else
+      max = index;
+    right = left + 1;
+    if (right <= pHeap->numData &&
+        pHeap->data[right] > pHeap->data[max])
+      max = right;
+    if (max != index) {
+      tmp = pHeap->data[index];
+      pHeap->data[index] = pHeap->data[max];
+      pHeap->data[max] = tmp;
+      heapify(max, pHeap);
+    }
+  } 
+}
+
+// la funzione di ordinamento sfrutta heapify
+void buildHeap(dataStruct *pHeap) {
+  int i;
+  for (i = (int)(pHeap->numData / 2); i > 0; --i)
+    heapify(i, pHeap);
+}
+
+int main () {
+
+  return 0;
+}
