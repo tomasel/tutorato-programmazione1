e9fb95365b3b0fb385c471b15b6f47ffbbed4120
tomasel <elia.tomasi@pm.me>
2023-12-04 09:12:51 +0100

    clang-format

--- 12-04/graph/main.cpp
+++ 12-04/graph/main.cpp
@@ -1,25 +1,23 @@
-#include <iostream>
 #include "Graph.h"
-
+#include <iostream>
 
 using namespace std;
 
+int main(int argc, char **argv) {
+  graph g;
+  if (argc < 2) {
+    cerr << "Missing input file" << endl;
+    exit(1);
+  }
+  fill_graph(g, argv[1]);
 
-int main(int argc, char** argv) {
-    graph g;
-    if (argc < 2){
-        cerr << "Missing input file" << endl;
-        exit(1);
-    }
-    fill_graph(g, argv[1]);
+  print_graph(g);
 
-    print_graph(g);
+  cout << "Calc dependecies for node: ";
+  int node;
+  cin >> node;
 
-    cout << "Calc dependecies for node: ";
-    int node;
-    cin >> node;
+  dependences(g, node);
 
-    dependences(g, node);
-
-    return 0;
+  return 0;
 }
